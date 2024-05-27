7a70e0579418a1fdba39934f02cc660e370ad306
Caleb Martin <calebmartin2020@gmail.com>
2024-05-27 17:12:54 -0500

    deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/{fcc_javascript_calculator}",
+  base: "/fcc_javascript_calculator",
   plugins: [react()],
 })
